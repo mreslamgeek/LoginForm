ff5d4aee14f0b21a8a79ca16499b2fe9acc3a2f8
Eslam Mohammed <42094349+mreslamgeek@users.noreply.github.com>
2019-04-26 21:16:16 +0200

    add TextFileds

--- lib/UI/login.dart
+++ lib/UI/login.dart
@@ -6,22 +6,51 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final TextEditingController _userController = new TextEditingController();
+  final TextEditingController _passController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Login"),
-        backgroundColor: Colors.yellow,
+        title: Text(
+          "Login",
+          style: TextStyle(color: Color.fromRGBO(17, 24, 34, 100)),
+        ),
+        backgroundColor: Colors.yellow[600],
+        centerTitle: true,
       ),
       backgroundColor: Color.fromRGBO(17, 24, 34, 100),
       body: Container(
         alignment: Alignment.topCenter,
         child: Column(
           children: <Widget>[
-            new Image.asset(
-              'images/lock.png',
-              height: 90.0,
-              width: 90.0,
+            Padding(
+              padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+              child: new Image.asset(
+                'images/lock.png',
+                height: 90.0,
+                width: 90.0,
+              ),
+            ),
+            Container(
+              width: 380.0,
+              height: 180.0,
+              color: Colors.yellow[600],
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    controller: _userController,
+                    decoration: InputDecoration(
+                        hintText: 'Username', icon: Icon(Icons.person)),
+                  ),
+                  TextField(
+                    controller: _passController,
+                    decoration: InputDecoration(
+                        hintText: 'Password', icon: Icon(Icons.lock)),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
